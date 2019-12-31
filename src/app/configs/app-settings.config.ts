573c3db94a4d4bb605a1a6c9791ef8258eb7036c
duycs <duycs@outlook.com>
2019-12-31 18:10:18 +0700

    update resource api

--- src/app/configs/app-settings.config.ts
+++ src/app/configs/app-settings.config.ts
@@ -1,6 +1,7 @@
 export const AppSettings = {
     //defaultBackendUrl: "http://localhost:5000/api/v1",
-    defaultBackendUrl: "https://poststudy-library.azurewebsites.net/api/v1",
+    //defaultBackendUrl: "https://poststudy-library.azurewebsites.net/api/v1",
+    defaultBackendUrl: "https://duycs-api-library.herokuapp.com/api/v1",
     defaultBackendProductionUrl: "https://poststudy-library.azurewebsites.net/api/v1",
     defaultBackendDevelopUrl: "http://localhost:5000/api/v1",
 }
