02ce84a82a44e15a0ad30ce560c3f0e5501b7689
duycs <duycs@outlook.com>
2020-02-13 11:20:52 +0700

    update connect

--- src/app/configs/app-settings.config.ts
+++ src/app/configs/app-settings.config.ts
@@ -1,6 +1,6 @@
 export const AppSettings = {
     //defaultBackendUrl: "http://localhost:5000/api/v1",
     defaultBackendUrl: "https://poststudy-library.azurewebsites.net/api/v1",
-    defaultBackendProductionUrl: "https://poststudy-library.azurewebsites.net/api/v1",
+    //defaultBackendProductionUrl: "https://poststudy-library.azurewebsites.net/api/v1",
     defaultBackendDevelopUrl: "http://localhost:5000/api/v1",
 }
